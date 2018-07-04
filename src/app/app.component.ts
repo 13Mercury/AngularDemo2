29f2c0cc183804bdf0cd7bdc445c92754de51644
13mercury <13mercuryh@gmail.com>
2018-07-04 00:28:54 -0700

    formsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '13Mercury';
+  name: string;
+  email: string;
+  website: string;
+  hobbies: string[];
+  showHobbies: boolean;
+
+  constructor() {
+    this.name = "Adolfo";
+    this.email = "Adolfo@mail.com";
+    this.website = "https://www.google.com"
+    this.hobbies = ['sol','luna','tierra'];
+    this.showHobbies = false;
+  }
+
+  toggleHobbies()
+  {
+    this.showHobbies = !this.showHobbies;
+  }
+
+  newHobby(hobby)
+  {
+    this.hobbies.push(hobby.value);
+    hobby.value = '';
+    return false;
+  }
+
 }
